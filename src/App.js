4db640d5b7a139bc7af93cf496344984bcf73c28
Abhinav Kumar Singh <abhinav.s@imimobile.com>
2020-01-08 12:48:24 +0530

    All updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as Sentry from '@sentry/browser';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function methodDoesNotExist(){
+  // eslint-disable-next-line no-throw-literal
+  try{
+    var err = 'Sentry test';
+    throw err;
+  }catch{
+    //nothing
+    Sentry.captureException(err);
+    console.log("DONE!!");
+  }
+};
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//       <button onClick={methodDoesNotExist}>Break the world</button>;
+//       <p>Form below</p>
+//       <form>
+//         NAME: <input type="text" id="name" placeholder="NAME" /> <br/>
+//         Email: <input type="email" id="email" placeholder="EMAIL" /><br/>
+//         COMMENT: <input type="text" id="comments" placeholder="COMMENT"/><br/>
+//         <button id="submit" onClick={s}>Send-TO-Sentry</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+function sendToSentry(){
+
+  // create a new XMLHttpRequest
+  var xhr = new XMLHttpRequest()
+
+  // get a callback when the server responds
+  // xhr.addEventListener('load', () => {
+  //   // update the state of the component with the result here
+  //   console.log(xhr.responseText)
+  // })
+  // open the request with the verb and the url
+  xhr.open('POST', 'https://sentry.io/api/0/projects/imi_org_slug/imi_proj_slug/user-feedback/');
+
+  xhr.setRequestHeader('Authorization','Bearer 1c0932a0af29448fb969324713fac8a7e09901c042e54c1f90a6475017e9cda8');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  
+  // send the request
+  xhr.send(JSON.stringify({ 
+    "comments": "Its broken",
+    "email":"bahin@shoa.com",
+    "event_id":"213cb4911fb5461191e1b97dac6aa230",
+    "name":"abhinav"
+  }))
+  console.log(xhr.responseText);
+  console.log("done");
+};
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+      <button onClick={methodDoesNotExist}>Break the world</button>;
+      <p>Form below</p>
+      <form>
+        NAME: <input type="text" id="name" placeholder="NAME" /> <br/>
+        Email: <input type="email" id="email" placeholder="EMAIL" /><br/>
+        COMMENT: <input type="text" id="comments" placeholder="COMMENT"/><br/>
+        <button type="button" id="submit" onClick={sendToSentry}>Send-TO-Sentry</button>
+      </form>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+    dsn: "https://701744cc1cc14210901d68a7f1103814@sentry.io/1872277",
+    beforeSend(event, hint) {
+        // Check if it is an exception, and if so, show the report dialog
+        if (event.exception) {
+          Sentry.showReportDialog({ eventId: event.event_id });
+        }
+        return event;
+      }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
